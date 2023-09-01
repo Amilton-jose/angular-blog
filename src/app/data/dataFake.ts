79fc32f8243c685e79b015d32c8920081f7a2a8a
Amilton José <amiltonjosefn2019@gmail.com>
2023-08-31 23:12:31 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,24 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "O que é java? dataFake",
+    "description": "dataFake",
+    "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffirecrux.com%2Fwp-content%2Fuploads%2F2019%2F07%2Fjava_logo-2.png&f=1&nofb=1&ipt=a031ae3e69d971f74ad2c1534dcd891caaf09abbc3a66cfa2760a7ac9c55690f&ipo=images"
+  },
+  {
+    "id": "2",
+    "title": "Como baixar e instalar o Java?",
+    "description": "desc",
+    "photoCover": "https://portal.trt3.jus.br/internet/servicos/pje/pje-em-casa-1/configure-o-acesso-ao-pje/windows/prepare-o-java/imagens/java-instalac3a7c3a3o-1.png/@@images/vG_cK6_wZ8-wzDzwq6o7eRmdyKmdiNmt8VnNaGiBn7ywL9i7DI1wLGlcjT-wrQ-ciW_BvG_hQ-CWvpE_EjBMINv3vfm-sjC0wJbJhjCIVtm3ijBNwpyJ7jBIVNl4iuBQwJbJUSC0eTlMwkBLw9F-.png"
+  },
+  {
+    "id": "3",
+    "title": "Configuração de variáveis de Ambientes!",
+    "description": "desc",
+    "photoCover": "https://www.oobj.com.br/bc/assets/Articles/29/VariaveisSistema.PNG"
+  }
+
+
+
+
+]
